db302c30eecd9de0ba5dccdd4e7d8d869021ae78
Michael Antiufieiev <antufeevmichael@gmail.com>
2024-12-21 04:05:36 +0200

    [ISSUE-3] setup keystore files

--- androidApp/build.gradle.kts
+++ androidApp/build.gradle.kts
@@ -1,7 +1,14 @@
 import me.bookk.build_src.constants.AndroidConfig
+import java.util.Properties
 
 plugins {
     alias(libs.plugins.bookk.android.application)
+}
+
+val keystoreProperties = file("keystore.properties").inputStream().use {
+    Properties().apply {
+        load(it)
+    }
 }
 
 android {
@@ -11,6 +18,30 @@ android {
         applicationId = AndroidConfig.ROOT_PACKAGE
         versionCode = AndroidConfig.VERSION_CODE
         versionName = AndroidConfig.VERSION_NAME
+    }
+
+    signingConfigs {
+        getByName("debug") {
+            keyAlias = keystoreProperties["debugKeyAlias"] as String
+            keyPassword = keystoreProperties["debugKeyPassword"] as String
+            storeFile = file(keystoreProperties["debugStoreFile"] as String)
+            storePassword = keystoreProperties["debugStorePassword"] as String
+        }
+        create("release") {
+            keyAlias = keystoreProperties["releaseKeyAlias"] as String
+            keyPassword = keystoreProperties["releaseKeyPassword"] as String
+            storeFile = file(keystoreProperties["releaseStoreFile"] as String)
+            storePassword = keystoreProperties["releaseStorePassword"] as String
+        }
+    }
+
+    buildTypes {
+        getByName("debug") {
+            signingConfig = signingConfigs.getByName("debug")
+        }
+        getByName("release") {
+            signingConfig = signingConfigs.getByName("release")
+        }
     }
 }
 
