c4186fcdce1bc96e2529737906ac447d046f70b8
Michael Antiufieiev <antufeevmichael@gmail.com>
2025-09-08 01:33:19 +0300

    [ISSUE-31] revert mock navigation

--- iosApp/iosApp/Feature/Authorization/Troubleshoot/TroubleshootScreen.swift
+++ iosApp/iosApp/Feature/Authorization/Troubleshoot/TroubleshootScreen.swift
@@ -15,7 +15,18 @@ struct TroubleshootScreen: View {
     
     
     var body: some View {
-		BusinessSettingsScreen()
+        let uiState = troubleshootVM.uiState
+        VStack {
+            TroubleshootCard(troubleshootInfo: uiState.troubleshootCardStaticData)
+            Spacer()
+            StateButton(state: uiState.contactSupportButton.impl()) {
+				//navigationStack.path.append()
+            }
+        }
+		.sendLifecycleEventsTo(viewModel: troubleshootVM)
+        .padding()
+        .navigationTitle(troubleshootVM.uiState.appBar.title.localized())
+        .navigationBarTitleDisplayMode(.large)
     }
 }
 
