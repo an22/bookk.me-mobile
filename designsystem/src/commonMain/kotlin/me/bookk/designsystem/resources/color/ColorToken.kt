dcf2739827193a51b3b7be7b8cb87e39c34a1e18
Michael Antiufieiev <antufeevmichael@gmail.com>
2026-04-26 14:52:53 +0200

    [ISSUE-31] android color resolver

---
+++ designsystem/src/commonMain/kotlin/me/bookk/designsystem/resources/color/ColorToken.kt
@@ -0,0 +1,20 @@
+package me.bookk.designsystem.resources.color
+
+enum class ColorToken {
+    Background,
+    PrimaryText,
+    SecondaryText,
+    HintText,
+    Elevated,
+    Divider,
+    ActionTextDisabled,
+    ActionText,
+    Inactive,
+    BlurredBackground,
+    Header,
+    Success,
+    InactiveToggle,
+    Button,
+    ButtonInactive,
+    ButtonActive,
+}
